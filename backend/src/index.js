26d3a838938b1590985a7a1a4cc14e9bae7bcd35
Sayan Das <sayan17102002@gmail.com>
2024-06-02 14:48:53 +0530

    index.js update

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,12 @@ dotenv.config({
     path: './.env'
 })
 
+const PORT = process.env.PORT || 8000
+
 connectDB()
 .then(() =>{
-    app.listen(process.env.PORT, () =>{
-        console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(PORT, () =>{
+        console.log(`Server is running on port ${PORT}`);
     })
 })
 .catch((err) =>{
